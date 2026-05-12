a02bb98d79c3468443419ffc2020420ab22ea5ac
devingrischow <devingrischow@outlook.com>
2026-05-12 11:12:51 -0500

    Added Shut Down Functions, Return to motor start functions, and set aboslute functions.
    In the open and get serial connection function absolute mode is now set, allowing for future pathing my x coordinates.

--- python/main/MotorMovementManagement.py
+++ python/main/MotorMovementManagement.py
@@ -8,7 +8,7 @@ class MotorMovementManagement:
     DEFAULT_SERIAL_PORT = '/dev/ttyUSB0'
     BAUD_RATE   = 115200
     MAX_TRAVEL_DISTANCE_MM = 100            # Max Travel Distance the motor can move (Value from side of motor)
-
+    SPEED       = 2000             # Feed rate in mm/min (start low, e.g. 1000-3000)
 
 # ================================================================
 
@@ -30,10 +30,10 @@ class MotorMovementManagement:
                 break
 
 
+
     def __send_gcode_command(self, ser, command_to_send):
         """Main Function that Sends GCode Command to a given Serial"""
         ser.write(command_to_send.encode('utf-8'))
-
 
     def __send_gcode_command_wait_for_timed_response(self, ser, command_to_send, time_to_wait=2):
         """Sends GCode Command and waits for a timed response"""
@@ -47,8 +47,16 @@ class MotorMovementManagement:
 
 
 
-    def __open_and_get_serial_connection(self):
-        """Opens a Connection to a serial connection, and returns the serial"""
+    def __set_serial_to_absolute_mode(self, ser):
+        """Sends a command that sets the serial to GCode Absolute Mode"""
+        absolute_command = "G90\n"
+
+        # Use the response from board to respond
+        self.__send_gcode_command_wait_for_response(ser, absolute_command)
+
+
+    def open_and_get_serial_connection(self):
+        """Opens a Connection to a serial connection, and returns the serial object"""
 
         print("Connecting to Benbox/GRBL...")
         ser = serial.Serial(self.SERIAL_PORT, self.BAUD_RATE, timeout=1)
@@ -58,8 +66,24 @@ class MotorMovementManagement:
         # Wake up GRBL
         flush_command = "\r\n\r\n"
         self.__send_gcode_command_wait_for_timed_response(ser, flush_command)
-        
         ser.flushInput()
+
+        self.__set_serial_to_absolute_mode(ser)
+
 
         return ser
 
+    def shut_down_motors(self, ser):
+        optional_disable_spindle_command = "M5\n"
+
+        self.__send_gcode_command_wait_for_response(ser, optional_disable_spindle_command)  # Optional: disable spindle (harmless here)
+        self.__set_serial_to_absolute_mode(ser)                                             # Back to absolute mode
+
+
+
+    def return_motor_to_start(self, ser):
+        """When called returns the motor back to the home position"""
+
+        return_to_start_gcode_command = "G1 X0 F{}\n".format(self.SPEED)
+
+        self.__send_gcode_command_wait_for_response(ser, return_to_start_gcode_command)
